1dd6bbd9d0481f23f582a9c11cbd73d6b04764f6
Aaron Robinson <aaronlrobinson@gmail.com>
2017-04-21 22:34:08 -0500

    Turn on preloading.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp, {
+      preloadModules: true
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
